2961465b1ef142216f8a4ee4f79da67ac9cf1707
Emily Woods <emilymonica.woods@gmail.com>
2017-08-19 22:14:55 +0100

    Restructure Application class

--- src/main/java/colouredLights/Application.java
+++ src/main/java/colouredLights/Application.java
@@ -1,24 +1,61 @@
 package colouredLights;
 
+import colouredLights.model.Light;
+import colouredLights.utils.TimeService;
+import colouredLights.utils.UserCommsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Scanner;
-
-import static java.lang.System.in;
-import static java.lang.System.out;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+    UserCommsService userComms;
+    TimeService timeService;
+    UserCommsManager userCommsManager;
+    LightStatePresenter lightStatePresenter;
+    Light light;
 
-    public static void main (String args[]) {
+    @Autowired
+    public Application(
+            UserCommsService userComms,
+            TimeService timeService,
+            UserCommsManager userCommsManager,
+            LightStatePresenter lightStatePresenter,
+            Light light
+    ) {
+
+        this.userComms = userComms;
+        this.timeService = timeService;
+        this.userCommsManager = userCommsManager;
+        this.lightStatePresenter = lightStatePresenter;
+        this.light = light;
+    }
+
+    public static void main(String args[]) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        UserComms userComms = new UserComms();
-        new UserCommsManager(userComms).getUserInput();
+        Integer userInput = userCommsManager.getUserInputInteger();
+
+        int currentPosition = 1;
+        int lightListElement = 0;
+        while (currentPosition <= userInput) {
+            if (lightListElement == light.colour.size()) lightListElement = 0;
+            outputState(currentPosition, lightListElement, Light.State.ON);
+            outputState(currentPosition, lightListElement, Light.State.OFF);
+            lightListElement += 1;
+            currentPosition += 1;
+        }
+    }
+
+    private void outputState(int currentPosition, int lightListElement, Light.State lightState) throws InterruptedException {
+        String lightOff = lightStatePresenter.presentLightState(timeService.currentTime(), currentPosition, lightState, light.colour.get(lightListElement));
+        userCommsManager.printLightState(lightOff);
+        TimeUnit.SECONDS.sleep(1);
     }
 }
