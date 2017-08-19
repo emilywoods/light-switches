46d58aa6913672245a8f2fe8fc18473f9a678755
Emily Woods <emilymonica.woods@gmail.com>
2017-08-19 15:50:53 +0100

    Create light state presenter. Update light model

---
+++ src/main/java/colouredLights/LightStatePresenter.java
@@ -0,0 +1,13 @@
+package colouredLights;
+
+import colouredLights.model.Light;
+import colouredLights.utils.TimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class LightStatePresenter {
+
+    public String getLightState(String s, Integer userInput, Light.State on, String s1) {
+        return "S";
+    }
+}
--- src/main/java/colouredLights/model/Light.java
+++ src/main/java/colouredLights/model/Light.java
@@ -1,14 +1,17 @@
 package colouredLights.model;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Light {
 
     public State state;
-    public List<String> colour;
+    public List<String> colour = Arrays.asList("Blue", "Green", "Red");
 
     public enum State {
         ON, OFF
     }
+
 
 }
